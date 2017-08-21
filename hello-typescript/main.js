31da439a169a0e687b0ffb3ead65bd161f7ad203
Leoxxid <leoxxid@hotmail.com>
2017-08-21 18:41:07 -0300

    Aula 04

---
+++ hello-typescript/main.js
@@ -0,0 +1,22 @@
+var minhaVariavel = 'Minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+// ES 6 ou ES 2015
+var num = 2;
+var pi = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); //ES 2015
+var Matematica = (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'abcd';
+n1 = 1;
