003acf78a76483e3ef9bee53cd2240c7e1ba9e97
Leoxxid <leoxxid@hotmail.com>
2017-08-25 02:09:51 -0300

    Aula 14 Output Properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -14,6 +14,7 @@ export class DataBindingComponent implements OnInit {
   valorSalvo: string ;
   isMouseOver: boolean = false;
   nomeDoCurso: string = 'Angular';
+  valorInicial: number = 15;
 
   getValor(){
   return 1;
@@ -37,6 +38,10 @@ export class DataBindingComponent implements OnInit {
 
   onMouseOverOut(){
     this.isMouseOver = !this.isMouseOver;
+  }
+
+  onMudouValor(evento){
+    console.log(evento.novoValor);
   }
 
   constructor() { }
